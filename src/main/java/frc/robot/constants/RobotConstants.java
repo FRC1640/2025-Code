5c19d2e1de42dfeeb6fc6144a5f371d121d76143
jdugan0 <justin.d.dugan@gmail.com>
2025-01-06 18:58:06 -0500

    swerve algorithms

--- src/main/java/frc/robot/constants/RobotConstants.java
+++ src/main/java/frc/robot/constants/RobotConstants.java
@@ -1,7 +1,30 @@
 package frc.robot.constants;
 
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.util.Units;
+
 public class RobotConstants {
+    public static enum PivotId {
+        FL, FR, BL, BR;
+    }
     public static class DriveConstants{
         public static double odometryFrequency = 200.0;
+        public static final double driveGearRatio = 116 / 15;
+        public static final double steerGearRatio = ((480 / 11)) * 1.0166667 * 0.99790377777778;
+        public static final double wheelYPos = Units.inchesToMeters(22.75 / 2);
+        public static final double wheelXPos = Units.inchesToMeters(22.75 / 2);
+        public static final double maxSpeed = 4.6;
+
+        private static final Translation2d frontLeftLocation = new Translation2d(wheelXPos, wheelYPos);
+        private static final Translation2d frontRightLocation = new Translation2d(wheelXPos, -wheelYPos);
+        public static final Translation2d backLeftLocation = new Translation2d(-wheelXPos, wheelYPos);
+        public static final Translation2d backRightLocation = new Translation2d(-wheelXPos, -wheelYPos);
+
+        public static final Translation2d[] positions = new Translation2d[] {
+            frontLeftLocation, frontRightLocation, backLeftLocation, backRightLocation };
+
+        public static final SwerveDriveKinematics kinematics = new SwerveDriveKinematics(
+            frontLeftLocation, frontRightLocation, backLeftLocation, backRightLocation);
     }
 }
--- src/main/java/frc/robot/subsystems/drive/Module.java
+++ src/main/java/frc/robot/subsystems/drive/Module.java
@@ -1,5 +1,9 @@
 package frc.robot.subsystems.drive;
 
+import frc.robot.constants.RobotConstants.PivotId;
+
 public class Module {
-    
+    ModuleIO io;
+    PivotId id;
+    ModuleIOInputsAutoLogged inputs = new ModuleIOInputsAutoLogged();
 }

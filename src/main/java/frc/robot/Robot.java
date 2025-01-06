f03eb601cc630c1380061401d44d6efcdf5148bd
jdugan0 <justin.d.dugan@gmail.com>
2025-01-05 21:29:15 -0500

    inital commit with akit

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,172 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.PowerDistribution;
+import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import org.littletonrobotics.junction.LogFileUtil;
+import org.littletonrobotics.junction.LoggedRobot;
+import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.networktables.NT4Publisher;
+import org.littletonrobotics.junction.wpilog.WPILOGReader;
+import org.littletonrobotics.junction.wpilog.WPILOGWriter;
+
+public class Robot extends LoggedRobot {
+  private Command m_autonomousCommand;
+
+  private final RobotContainer m_robotContainer;
+
+  public static enum Mode {
+    REAL,
+    SIM,
+    REPLAY
+  };
+
+  public Robot() {
+    Logger.recordMetadata("ProjectName", BuildConstants.MAVEN_NAME);
+    Logger.recordMetadata("BuildDate", BuildConstants.BUILD_DATE);
+    Logger.recordMetadata("GitSHA", BuildConstants.GIT_SHA);
+    Logger.recordMetadata("GitDate", BuildConstants.GIT_DATE);
+    Logger.recordMetadata("GitBranch", BuildConstants.GIT_BRANCH);
+    Logger.recordMetadata("RuntimeType", getRuntimeType().toString());
+    Logger.recordMetadata("RobotMode", getMode().toString());
+    // Logger.recordMetadata("MACAddress", getMACAddress());
+    switch (BuildConstants.DIRTY) {
+      case 0:
+        Logger.recordMetadata("GitDirty", "All changes committed");
+        break;
+      case 1:
+        Logger.recordMetadata("GitDirty", "Uncomitted changes");
+        break;
+      default:
+        Logger.recordMetadata("GitDirty", "Unknown");
+        break;
+    }
+    System.out.println(getMode().toString());
+    // Set up data receivers & replay source
+    switch (getMode()) {
+        // Running on a real robot, log to a USB stick
+      case REAL:
+        new PowerDistribution(1, ModuleType.kRev);
+        Logger.addDataReceiver(new WPILOGWriter());
+        Logger.addDataReceiver(new NT4Publisher());
+        break;
+
+        // Running a physics simulator, log to local folder
+      case SIM:
+        Logger.addDataReceiver(new WPILOGWriter("logs"));
+        Logger.addDataReceiver(new NT4Publisher());
+        break;
+
+        // Replaying a log, set up replay source
+      case REPLAY:
+        setUseTiming(false); // Run as fast as possible
+        String logPath = LogFileUtil.findReplayLog();
+        Logger.setReplaySource(new WPILOGReader(logPath));
+        Logger.addDataReceiver(new WPILOGWriter(LogFileUtil.addPathSuffix(logPath, "_sim")));
+        break;
+    }
+
+    // See http://bit.ly/3YIzFZ6 for more information on timestamps in AdvantageKit.
+    // Logger.getInstance().disableDeterministicTimestamps()
+
+    // Start AdvantageKit Logger
+    Logger.start();
+
+    m_robotContainer = new RobotContainer();
+  }
+
+  @Override
+  public void robotPeriodic() {
+    CommandScheduler.getInstance().run();
+  }
+
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  @Override
+  public void disabledExit() {}
+
+  @Override
+  public void autonomousInit() {
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.schedule();
+    }
+  }
+
+  @Override
+  public void autonomousPeriodic() {}
+
+  @Override
+  public void autonomousExit() {}
+
+  @Override
+  public void teleopInit() {
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.cancel();
+    }
+  }
+
+  @Override
+  public void teleopPeriodic() {}
+
+  @Override
+  public void teleopExit() {}
+
+  @Override
+  public void testInit() {
+    CommandScheduler.getInstance().cancelAll();
+  }
+
+  @Override
+  public void testPeriodic() {}
+
+  @Override
+  public void testExit() {}
+
+  public static boolean isReplay() {
+    String replay = System.getProperty("REPLAY");
+    return replay != null && replay.toLowerCase().equals("true");
+  }
+
+  public static Mode getMode() {
+    if (isReal()) {
+      return Mode.REAL;
+    }
+
+    if (isReplay()) {
+      return Mode.REPLAY;
+    }
+
+    return Mode.SIM;
+  }
+
+  public static String getMACAddress() {
+    try {
+      InetAddress localHost = InetAddress.getLocalHost();
+      NetworkInterface ni = NetworkInterface.getByInetAddress(localHost);
+      byte[] hardwareAddress = ni.getHardwareAddress();
+      String[] hexadecimal = new String[hardwareAddress.length];
+      for (int i = 0; i < hardwareAddress.length; i++) {
+        hexadecimal[i] = String.format("%02X", hardwareAddress[i]);
+      }
+      return String.join("-", hexadecimal);
+    } catch (UnknownHostException | SocketException e) {
+      e.printStackTrace();
+      return "Not found";
+    }
+  }
+}

9662b0c23b807c4268ac1f1209b8eb2c9519f5f3
jdugan0 <justin.d.dugan@gmail.com>
2025-01-12 17:41:40 -0500

    new dream level and faster pid

--- src/main/java/frc/robot/constants/RobotPIDConstants.java
+++ src/main/java/frc/robot/constants/RobotPIDConstants.java
@@ -20,7 +20,7 @@ public class RobotPIDConstants {
 
   public static final PIDConstants steerPID = new PIDConstants(0.725, 0.0, 0.005);
 
-  public static final PIDConstants linearDrivePID = new PIDConstants(0.6, 0, 0);
+  public static final PIDConstants linearDrivePID = new PIDConstants(0.8, 0, 0);
 
-  public static final PIDConstants rotateToAnglePIDRadians = new PIDConstants(0.3, 0.001, 0.0001);
+  public static final PIDConstants rotateToAnglePIDRadians = new PIDConstants(0.5, 0.001, 0.0001);
 }

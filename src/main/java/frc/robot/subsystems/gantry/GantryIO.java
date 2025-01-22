14dfd43de354a5d96ab33952080a70c1f6c7cac8
Joshua Simms <Boxtopy755@gmail.com>
2025-01-22 18:31:15 -0500

    Gantry stuff :))))))))

--- src/main/java/frc/robot/subsystems/gantry/GantryIO.java
+++ src/main/java/frc/robot/subsystems/gantry/GantryIO.java
@@ -6,12 +6,12 @@ public interface GantryIO extends AutoCloseable {
   @AutoLog
   public class GantryIOInputs {
     public int rawEncoderValue;
-    public double carriageEncoderVoltage;
-    public double carriageEncoderRelative;
+    public double encoderVoltage;
+    public double encoderRelative;
 
-    public double carriageTempCelcius;
-    public double carriageAppliedVoltage;
-    public double driveCurrentAmps;
+    public double tempCelcius;
+    public double appliedVoltage;
+    public double currentAmps;
   }
 
   public default void updateInputs(GantryIOInputs inputs) {}

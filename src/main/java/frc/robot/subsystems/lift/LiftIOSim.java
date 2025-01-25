13de914d3e9787816483864bc11b1c90d74acaed
SpeedSlicer <mathewshaun25@gmail.com>
2025-01-25 18:47:48 -0500

    ad

--- src/main/java/frc/robot/subsystems/lift/LiftIOSim.java
+++ src/main/java/frc/robot/subsystems/lift/LiftIOSim.java
@@ -1,9 +1,15 @@
 package frc.robot.subsystems.lift;
 
+import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+
 public class LiftIOSim implements LiftIO {
-  // private final DCMotorSim motor1Sim;
-  // private final DCMotorSim motor2Sim;
-  public LiftIOSim() {}
+  private final DCMotorSim motor1Sim;
+  private final DCMotorSim motor2Sim;
+
+  public LiftIOSim() {
+    motor1Sim = new DCMotorSim();
+    motor2Sim = new DCMotorSim();
+  }
 
   @Override
   public void updateInputs(LiftIOInputs inputs) {}
--- src/main/java/frc/robot/subsystems/lift/LiftIOSpark.java
+++ src/main/java/frc/robot/subsystems/lift/LiftIOSpark.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems.lift;
 
 import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkMax;
 import frc.robot.constants.RobotConstants.LiftConstants;
 import frc.robot.constants.SparkConstants;
 import frc.robot.util.spark.SparkConfigurer;
@@ -8,10 +9,18 @@ import frc.robot.util.spark.SparkConfigurer;
 public class LiftIOSpark implements LiftIO {
   RelativeEncoder motorEncoder1;
   RelativeEncoder motorEncoder2;
+  SparkMax motor1;
+  SparkMax motor2;
 
   public LiftIOSpark() {
-    SparkConfigurer.configSparkMax(SparkConstants.getDefaultMax(LiftConstants.liftMotor1ID, false));
-    SparkConfigurer.configSparkMax(SparkConstants.getDefaultMax(LiftConstants.liftMotor2ID, false));
+    motor1 =
+        SparkConfigurer.configSparkMax(
+            SparkConstants.getDefaultMax(LiftConstants.liftMotor1ID, false));
+    motor2 =
+        SparkConfigurer.configSparkMax(
+            SparkConstants.getDefaultMax(LiftConstants.liftMotor2ID, false));
+    motorEncoder1 = motor1.getEncoder();
+    motorEncoder2 = motor2.getEncoder();
   }
 
   @Override

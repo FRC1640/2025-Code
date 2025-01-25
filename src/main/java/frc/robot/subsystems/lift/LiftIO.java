0d873bad8070686641eebb17550b6bce4be6215a
Kgupta011 <gimonika82@gmail.com>
2025-01-25 21:06:04 +0000

    fixed errors

--- src/main/java/frc/robot/subsystems/lift/LiftIO.java
+++ src/main/java/frc/robot/subsystems/lift/LiftIO.java
@@ -1,10 +1,19 @@
-package frc.robot.subsystems.lift;
-
 import org.littletonrobotics.junction.AutoLog;
 
 public interface LiftIO extends AutoCloseable {
   @AutoLog
-  public static class LiftIOInputs {}
+  public static class LiftIOInputs {
+    public double motor1Position = 0.0;  
+    public double motor2Position = 0.0;   
+    public double motor1Velocity = 0.0;   
+    public double motor2Velocity = 0.0;   
+    public double motor1Current = 0.0;    
+    public double motor2Current = 0.0;    
+    public double motor1Voltage = 0.0;    
+    public double motor2Voltage = 0.0;   
+    public boolean topLimitSwitch = false; 
+    public boolean bottomLimitSwitch = false; 
+  }
 
   public default void updateInputs(LiftIOInputs inputs) {}
 

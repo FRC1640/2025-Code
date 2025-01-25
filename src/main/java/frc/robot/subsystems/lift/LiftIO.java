47bbf022b68740f5225cfcbc83cbcc43ba20794c
SpeedSlicer <mathewshaun25@gmail.com>
2025-01-25 17:12:21 -0500

    YE

--- src/main/java/frc/robot/subsystems/lift/LiftIO.java
+++ src/main/java/frc/robot/subsystems/lift/LiftIO.java
@@ -1,17 +1,18 @@
+package frc.robot.subsystems.lift;
+
 import org.littletonrobotics.junction.AutoLog;
 
 public interface LiftIO extends AutoCloseable {
   @AutoLog
   public static class LiftIOInputs {
-    public double liftmotor1Position = 0.0;  
-    public double lift = 0.0;   
-    public double liftmotor1Velocity = 0.0;   
-    public double liftmotor2Velocity = 0.0;   
-    public double liftmotor1Current = 0.0;    
-    public double liftmotor2Current = 0.0;    
-    public double liftmotor1Voltage = 0.0;    
-    public double liftmotor2Voltage = 0.0;   
-  
+    public double liftmotor1Position = 0.0;
+    public double lift = 0.0;
+    public double liftmotor1Velocity = 0.0;
+    public double liftmotor2Velocity = 0.0;
+    public double liftmotor1Current = 0.0;
+    public double liftmotor2Current = 0.0;
+    public double liftmotor1Voltage = 0.0;
+    public double liftmotor2Voltage = 0.0;
   }
 
   public default void updateInputs(LiftIOInputs inputs) {}

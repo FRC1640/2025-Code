f523d97b9b9d42f79daa5761b19401c48b64e287
zoe99999999 <zoe@ebers.com>
2025-01-25 19:54:42 -0500

    vendor dep installed, lasercan added to Robot, empty IOLaserCAN

---
+++ src/main/java/frc/robot/sensors/reefdetector/ReefDetectorIOLaserCAN.java
@@ -0,0 +1,7 @@
+package frc.robot.sensors.reefdetector;
+
+public class ReefDetectorIOLaserCAN implements ReefDetectorIO {
+  public ReefDetectorIOLaserCAN(int channel) {
+    // set source!
+  }
+}

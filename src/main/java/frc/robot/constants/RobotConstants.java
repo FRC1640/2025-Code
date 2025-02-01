e97fceaaba1aaa07f0bca5072d910d8a13cde659
jdugan0 <justin.d.dugan@gmail.com>
2025-02-01 14:36:53 -0500

    gantry gear ratio

--- src/main/java/frc/robot/constants/RobotConstants.java
+++ src/main/java/frc/robot/constants/RobotConstants.java
@@ -138,7 +138,7 @@ public class RobotConstants {
 
   public static class GantryConstants {
     public static final int gantrySparkID = 13; // UPDATE
-    public static final double gantryGearRatio = 10; // UPDATE
+    public static final double gantryGearRatio = 27;
     public static final double pulleyRadiusIn = .5; // inches for now / placeholder
     // left -> right limit
     public static final Limit gantryLimits = new Limit(0.0, 12.0);

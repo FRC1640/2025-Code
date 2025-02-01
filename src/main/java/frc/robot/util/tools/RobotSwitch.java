4a2d0c42ec8329651ea1bf839d54024f7a672785
SpeedSlicer <mathewshaun25@gmail.com>
2025-02-01 14:45:26 -0500

    I disagree

--- src/main/java/frc/robot/util/tools/RobotSwitch.java
+++ src/main/java/frc/robot/util/tools/RobotSwitch.java
@@ -3,11 +3,18 @@ package frc.robot.util.tools;
 import frc.robot.constants.RobotConstants.RobotConfigConstants;
 
 public class RobotSwitch {
+
   public enum RobotType {
     Duex25,
     Prime25,
   }
-
+  /**
+   * @param <T> Any Type of Data
+   * @param duex25 Returns if it is the Duex 2025 robot
+   * @param prime25 Returns this value if it is Prime 2025 robot
+   * @return returns duex25 parm if it is Duex 2025 robot, returns prime25 parameter if it is Prime
+   *     2025 robot in the config
+   */
   public static <T> T robotTypeValue(T duex25, T prime25) {
     switch (RobotConfigConstants.robotType) {
       case Duex25:

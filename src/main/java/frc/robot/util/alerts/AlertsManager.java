caa2542f2f7ad51f539260c8cb735ad04343aa95
SpeedSlicer <mathewshaun25@gmail.com>
2025-02-03 16:23:25 -0500

    a

--- src/main/java/frc/robot/util/alerts/AlertsManager.java
+++ src/main/java/frc/robot/util/alerts/AlertsManager.java
@@ -2,9 +2,7 @@ package frc.robot.util.alerts;
 
 import edu.wpi.first.wpilibj.Alert;
 import edu.wpi.first.wpilibj.Alert.AlertType;
-import frc.robot.util.dashboard.Elastic;
-import frc.robot.util.dashboard.Elastic.Notification;
-import frc.robot.util.dashboard.Elastic.Notification.NotificationLevel;
+import frc.robot.util.notification.NotificationManager;
 import frc.robot.util.periodic.PeriodicBase;
 import java.util.ArrayList;
 import java.util.function.BooleanSupplier;
@@ -13,10 +11,6 @@ public class AlertsManager extends PeriodicBase {
 
   private static ArrayList<Alert> alerts = new ArrayList<Alert>();
   private static ArrayList<BooleanSupplier> alertConditions = new ArrayList<BooleanSupplier>();
-  public static ArrayList<Notification> notificationQueue = new ArrayList<Notification>();
-  private static ArrayList<String> ranNotifications = new ArrayList<String>();
-  private int notificationQueueIndex = 0;
-  private Long currentNotificationDelay = Long.valueOf(0);
 
   public AlertsManager() {}
 
@@ -33,61 +27,14 @@ public class AlertsManager extends PeriodicBase {
     alertConditions.add(condition);
   }
 
-  private long lastNotificationTime = 0;
-
   @Override
   public void periodic() {
 
     for (int i = 0; i < alerts.size(); i++) {
       boolean alertGet = alertConditions.get(i).getAsBoolean();
-
-      if (alertGet == true) {
-        NotificationLevel notifLevel;
-
-        switch (alerts.get(i).getType()) {
-          case kError:
-            notifLevel = NotificationLevel.ERROR;
-            break;
-          case kWarning:
-            notifLevel = NotificationLevel.WARNING;
-            break;
-          case kInfo:
-            notifLevel = NotificationLevel.INFO;
-            break;
-          default:
-            notifLevel = NotificationLevel.WARNING;
-        }
-        Notification notif =
-            new Notification(
-                notifLevel, notifLevel + " - " + alerts.get(i).getText(), alerts.get(i).getText());
-        if (!ranNotifications.contains(notif.getTitle())) {
-          notificationQueue.add(notif);
-          ranNotifications.add(notif.getTitle());
-        } else {
-        }
-      }
+      NotificationManager.alertNotification(alertGet, alerts.get(i));
 
       alerts.get(i).set(alertGet);
     }
-    // sending notifications
-    if (!notificationQueue.isEmpty()
-        && notificationQueueIndex < notificationQueue.size()
-        && notificationQueue.get(notificationQueueIndex) != null
-        && activeNotification(currentNotificationDelay)) {
-
-      Elastic.sendNotification(notificationQueue.get(notificationQueueIndex));
-      currentNotificationDelay =
-          Long.valueOf(notificationQueue.get(notificationQueueIndex).getDisplayTimeMillis());
-      notificationQueueIndex++;
-    }
-  }
-
-  public Boolean activeNotification(Long notificationDelay) {
-    long currentTime = System.currentTimeMillis();
-    if (currentTime - lastNotificationTime + 0.01 >= notificationDelay) {
-      lastNotificationTime = currentTime;
-      return true;
-    }
-    return false;
   }
 }

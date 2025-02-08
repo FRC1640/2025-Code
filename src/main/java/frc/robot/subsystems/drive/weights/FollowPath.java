ab462cbde31289c36c2d14317288924ddac9a2df
jdugan0 <justin.d.dugan@gmail.com>
2025-02-08 13:05:06 -0500

    follow path refactor

--- src/main/java/frc/robot/subsystems/drive/weights/FollowPath.java
+++ src/main/java/frc/robot/subsystems/drive/weights/FollowPath.java
@@ -13,35 +13,20 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.constants.RobotConstants.DriveConstants;
 import frc.robot.sensors.gyro.Gyro;
-import frc.robot.subsystems.drive.DriveSubsystem;
-import frc.robot.util.tools.DistanceManager;
 import java.util.List;
 import java.util.function.BooleanSupplier;
-import java.util.function.Function;
 import java.util.function.Supplier;
 
 public class FollowPath {
-  private Supplier<Pose2d> robotPose;
-  private Supplier<Pose2d[]> targetPoses;
-  private Gyro gyro;
-  private Function<Pose2d, Pose2d> poseFunction;
+  protected Supplier<Pose2d> robotPose;
+  protected Gyro gyro;
 
-  Command pathCommand = null;
-  private DriveSubsystem driveSubsystem;
-  private Pose2d[] pose2dArray;
+  protected Command pathCommand = null;
+  protected Pose2d[] pose2dArray;
 
-  public FollowPath(
-      Supplier<Pose2d> robotPose,
-      Supplier<Pose2d[]> targetPoses,
-      Gyro gyro,
-      Function<Pose2d, Pose2d> poseFunction,
-      DriveSubsystem driveSubsystem,
-      Pose2d[] pose2dArray) {
+  public FollowPath(Supplier<Pose2d> robotPose, Gyro gyro, Pose2d[] pose2dArray) {
     this.robotPose = robotPose;
-    this.targetPoses = targetPoses;
     this.gyro = gyro;
-    this.poseFunction = poseFunction;
-    this.driveSubsystem = driveSubsystem;
     this.pose2dArray = pose2dArray;
   }
 
@@ -77,20 +62,5 @@ public class FollowPath {
     return new Trigger(condition)
         .onTrue(new InstantCommand(() -> startPath()))
         .onFalse(new InstantCommand(() -> stopPath()));
-  }
-
-  private Pose2d getNearestTarget() {
-    if (poseFunction != null) {
-      return DistanceManager.getNearestPosition(robotPose.get(), targetPoses.get(), poseFunction);
-    }
-    return DistanceManager.getNearestPosition(robotPose.get(), targetPoses.get());
-  }
-
-  public double getTargetDistance() {
-    return robotPose.get().getTranslation().getDistance(getNearestTarget().getTranslation());
-  }
-
-  public Rotation2d getAngleDistance() {
-    return getNearestTarget().getRotation().minus(robotPose.get().getRotation());
   }
 }

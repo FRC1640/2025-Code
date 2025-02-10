c50e47403a3126c0ff9adb2cd7abf5a6eb5e5d0f
Joshua Simms <Boxtopy755@gmail.com>
2025-02-10 18:55:00 -0500

    the basics

---
+++ src/main/java/frc/robot/util/tools/logging/VelocityLogStorage.java
@@ -0,0 +1,33 @@
+package frc.robot.util.tools.logging;
+
+import java.util.function.DoubleSupplier;
+import org.littletonrobotics.junction.Logger;
+
+public class VelocityLogStorage implements Loggable {
+  private DoubleSupplier currentVelocity;
+
+  public DoubleSupplier getCurrentVelocity() {
+    return currentVelocity;
+  }
+
+  private DoubleSupplier setpointVelocity;
+  private String name;
+
+  public DoubleSupplier getSetpointVelocity() {
+    return setpointVelocity;
+  }
+
+  public VelocityLogStorage(
+      DoubleSupplier currentVelocity, DoubleSupplier setpointVelocity, String name) {
+    this.currentVelocity = currentVelocity;
+    this.setpointVelocity = setpointVelocity;
+    this.name = name;
+  }
+
+  @Override
+  public void log() {
+    Logger.recordOutput("VelocityLogging/" + name + "/setpoint", setpointVelocity.getAsDouble());
+    Logger.recordOutput(
+        "VelocityLogging/" + name + "/currentVelocity", setpointVelocity.getAsDouble());
+  }
+}

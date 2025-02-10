8b9ae0537ad993920e1f6f07d88d2721620e26b0
jdugan0 <justin.d.dugan@gmail.com>
2025-02-10 15:13:25 -0500

    spotless fix...

--- src/main/java/frc/robot/sensors/odometry/RobotOdometry.java
+++ src/main/java/frc/robot/sensors/odometry/RobotOdometry.java
@@ -48,7 +48,8 @@ public class RobotOdometry extends PeriodicBase {
       visionMap.put(aprilTagVision.getCameraName(), aprilTagVision);
     }
     SparkOdometryThread.getInstance().start();
-    OdometryStorage main = branchEstimator("Main", new AprilTagVision[0], VisionUpdateMode.PHOTONVISION);
+    OdometryStorage main =
+        branchEstimator("Main", new AprilTagVision[0], VisionUpdateMode.PHOTONVISION);
     OdometryStorage mainTrig = branchEstimator("MainTrig", cameras, VisionUpdateMode.TRIG);
     mainTrig.setTrustedRotation(main);
   }

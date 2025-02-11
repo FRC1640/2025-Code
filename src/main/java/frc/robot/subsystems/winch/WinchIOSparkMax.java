146138bba73744b66c23a060acbd2870b271c900
tunafish116 <derrickyu10001@gmail.com>
2025-02-10 21:04:38 -0500

    commit

--- src/main/java/frc/robot/subsystems/winch/WinchIOSparkMax.java
+++ src/main/java/frc/robot/subsystems/winch/WinchIOSparkMax.java
@@ -1,20 +1,38 @@
 package frc.robot.subsystems.winch;
 
 import com.revrobotics.spark.SparkMax;
-
+import edu.wpi.first.math.controller.PIDController;
 import frc.robot.constants.RobotConstants.ClimberConstants;
-import frc.robot.subsystems.climber.ClimberIO.ClimberIOInputs;
+import frc.robot.constants.RobotPIDConstants;
+import frc.robot.constants.SparkConstants;
+import frc.robot.sensors.resolvers.ResolverVoltage;
+import frc.robot.util.spark.SparkConfigurer;
 import frc.robot.util.tools.MotorLim;
 
-public class WinchIOSparkMax implements WinchIO{
-  
+public class WinchIOSparkMax implements WinchIO {
+
+  private final ResolverVoltage winchEncoder;
   private final SparkMax winchLeaderSpark;
   private final SparkMax winchFollowerSpark;
+  private final PIDController winchPID =
+      RobotPIDConstants.constructPID(RobotPIDConstants.climberWinchPID);
+
+  public WinchIOSparkMax() {
+    winchLeaderSpark =
+        SparkConfigurer.configSparkMax(
+            SparkConstants.getDefaultMax(ClimberConstants.climberWinch1MotorID, false));
+    winchFollowerSpark =
+        SparkConfigurer.configSparkMax(
+            SparkConstants.getDefaultMax(ClimberConstants.climberWinch2MotorID, false),
+            winchLeaderSpark);
+
+    winchEncoder = new ResolverVoltage(ClimberConstants.winchResolverInfo);
+  }
   /*
    * Set voltage of the winch motors
    */
   @Override
-  public void setClimberWinchVoltage(double voltage, ClimberIOInputs inputs) {
+  public void setClimberWinchVoltage(double voltage, WinchIOInputs inputs) {
     winchLeaderSpark.setVoltage(
         MotorLim.clampVoltage(
             MotorLim.applyLimits(
@@ -24,8 +42,21 @@ public class WinchIOSparkMax implements WinchIO{
    * Sets the position of the winch motors using a PID
    */
   @Override
-  public void setClimberWinchPosition(double position, ClimberIOInputs inputs) {
+  public void setClimberWinchPosition(double position, WinchIOInputs inputs) {
     setClimberWinchVoltage(
-        MotorLim.clampVoltage(winchPID.calculate(inputs.liftMotorPosition, position)), inputs);
+        MotorLim.clampVoltage(winchPID.calculate(inputs.winchLeaderMotorPosition, position)),
+        inputs);
+  }
+
+  @Override
+  public void updateInputs(WinchIOInputs inputs) {
+    inputs.winchLeaderMotorPosition =
+        winchEncoder.getDegrees(); // says degrees but really in meters
+    inputs.winchLeaderMotorCurrent = winchLeaderSpark.getOutputCurrent();
+    inputs.winchFollowerMotorCurrent = winchFollowerSpark.getOutputCurrent();
+    inputs.winchLeaderMotorVoltage = winchLeaderSpark.getAppliedOutput();
+    inputs.winchFollowerMotorVoltage = winchFollowerSpark.getAppliedOutput();
+    inputs.winchLeaderMotorTemperature = winchLeaderSpark.getMotorTemperature();
+    inputs.winchFollowerMotorTemperature = winchFollowerSpark.getMotorTemperature();
   }
 }

8720ae28572baa09643e5f879ed00b7edb2229f2
jdugan0 <justin.d.dugan@gmail.com>
2025-02-12 16:49:35 -0500

    fixed imports

--- src/main/java/frc/robot/subsystems/lift/LiftSubsystem.java
+++ src/main/java/frc/robot/subsystems/lift/LiftSubsystem.java
@@ -6,7 +6,6 @@ import static edu.wpi.first.units.Units.Volts;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
-import frc.robot.constants.RobotConstants.LiftConstants.CoralPreset;
 import frc.robot.util.sysid.SimpleMotorSysidRoutine;
 import org.littletonrobotics.junction.Logger;
 import org.littletonrobotics.junction.mechanism.LoggedMechanism2d;

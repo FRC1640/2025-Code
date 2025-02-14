e0a18c0a319b480d8711f15359f9f22280780487
tommym78 <mtomaszewicz78@gmail.com>
2025-02-13 21:04:01 -0500

    PAIEUGFOAWEPUFH

--- src/main/java/frc/robot/subsystems/drive/weights/FollowPathNearest.java
+++ src/main/java/frc/robot/subsystems/drive/weights/FollowPathNearest.java
@@ -1,8 +1,15 @@
 package frc.robot.subsystems.drive.weights;
 
+import com.pathplanner.lib.config.PIDConstants;
 import com.pathplanner.lib.path.PathConstraints;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import frc.robot.constants.RobotConstants.DriveConstants;
+import frc.robot.constants.RobotPIDConstants;
 import frc.robot.sensors.gyro.Gyro;
 import frc.robot.subsystems.drive.DriveSubsystem;
 import frc.robot.util.tools.DistanceManager;
@@ -12,6 +19,8 @@ import java.util.function.Supplier;
 public class FollowPathNearest extends FollowPath {
   Supplier<Pose2d[]> positions;
   Function<Pose2d, Pose2d> poseFunction;
+  private PIDController rotPid =
+      RobotPIDConstants.constructPID(RobotPIDConstants.angleFollowPath, "FollowPathNearest");
 
   public FollowPathNearest(
       Supplier<Pose2d> robotPose,
@@ -22,8 +31,8 @@ public class FollowPathNearest extends FollowPath {
       DriveSubsystem driveSubsystem) {
     super(robotPose, gyro, null, pathConstraints, null, driveSubsystem);
     this.positions = positions;
-    pose2dArray = new Pose2d[] {findNearest(positions.get())};
-    endRotation = findNearest(positions.get()).getRotation();
+    pose2dArray = new Pose2d[] {findNearest(this.positions.get())};
+    endRotation = findNearest(this.positions.get()).getRotation();
     this.poseFunction = poseFunction;
   }
 
@@ -41,22 +50,26 @@ public class FollowPathNearest extends FollowPath {
   @Override
   public void startPath() {
     Pose2d nearestPos =
-        new Pose2d(
-            findNearest(positions.get()).getTranslation(),
-            findNearest(positions.get()).getRotation());
+        new Pose2d(findNearest(positions.get()).getTranslation(), findNearest(positions.get()).getRotation());
 
-    double v = nearestPos.getTranslation().getDistance(robotPose.get().getTranslation());
-    Rotation2d angle =
-        nearestPos.getTranslation().minus(robotPose.get().getTranslation()).getAngle();
-
-    double midPointLength = Math.abs(angle.getCos() * v);
-
-    Pose2d midPoint =
-        DistanceManager.addRotatedDim(nearestPos, midPointLength * 0.5, nearestPos.getRotation());
-
-    pose2dArray = new Pose2d[] {midPoint, nearestPos};
+    pose2dArray = new Pose2d[] {nearestPos};
     endRotation = findNearest(positions.get()).getRotation();
 
+    PathplannerWeight.overrideRotation(()->omegaOverride(()->nearestPos.getRotation()));
+
     super.startPath();
+  }
+
+  @Override
+  public void stopPath(){
+    PathplannerWeight.clearRotationOverride();
+    super.stopPath();
+  }
+
+  public double omegaOverride(Supplier<Rotation2d> angle) {
+    double pid = rotPid.calculate(robotPose.get().getRotation().minus(angle.get()).getRadians(), 0);
+    pid = MathUtil.clamp(pid, -1, 1);
+    // pid *= DriveConstants.maxOmega;
+    return pid;
   }
 }

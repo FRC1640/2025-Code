b40d2e2339ba51a175a24dfabfa6999089f186ef
SpeedSlicer <mathewshaun25@gmail.com>
2025-02-15 19:54:09 -0500

    JakeGPT > ChatGPT o3

--- src/main/java/frc/robot/util/tools/WPICal/WPICALPosManager.java
+++ src/main/java/frc/robot/util/tools/WPICal/WPICALPosManager.java
@@ -3,10 +3,12 @@ package frc.robot.util.tools.WPICal;
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.Alert.AlertType;
+import edu.wpi.first.wpilibj.Filesystem;
 import frc.robot.constants.RobotConstants.AprilTagPositionSettings;
 import frc.robot.util.alerts.AlertsManager;
 import frc.robot.util.tools.WPICal.AprilTagPositionSwitcher.AprilTagSetting;
 import java.io.IOException;
+import java.nio.file.Path;
 
 public class WPICALPosManager {
   public static AprilTagFieldLayout aprilTagFieldLayout;
@@ -18,13 +20,17 @@ public class WPICALPosManager {
           "Error - WPICal JSON file does not exist",
           AlertType.kError);
       try {
+
         aprilTagFieldLayout =
-            AprilTagFieldLayout.loadFromResource(AprilTagPositionSettings.WPICalOutputJson);
+            new AprilTagFieldLayout(
+                Path.of(
+                    Filesystem.getDeployDirectory()
+                        + "/resources/"
+                        + AprilTagPositionSettings.WPICalOutputJson));
       } catch (IOException e) {
         e.printStackTrace();
         aprilTagFieldLayout = null;
       }
-      aprilTagFieldLayout = null;
     }
   }
 

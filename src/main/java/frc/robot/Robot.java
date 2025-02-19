6ea99ff13c725d72b107f72fc9f57bb6040a92dd
SpeedSlicer <mathewshaun25@gmail.com>
2025-02-18 21:05:05 -0500

    ITS SOMETHING TO DO WITH DEFAULT COMMANDS

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,6 +10,7 @@ import edu.wpi.first.net.WebServer;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.PowerDistribution;
 import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.constants.RobotConstants.MotorInfo;
@@ -192,6 +193,8 @@ public class Robot extends LoggedRobot {
         CommandScheduler.getInstance().getActiveButtonLoop().clear();
         break;
       default:
+        LiveWindow.setEnabled(false);
+        CommandScheduler.getInstance().enable();
         break;
     }
   }

fd298e88603ff0fe7d8babd42cdebbb7fffdded9
jdugan0 <justin.d.dugan@gmail.com>
2025-02-25 12:00:23 -0500

    changed max dist from target setpoint

--- src/main/java/frc/robot/constants/RobotConstants.java
+++ src/main/java/frc/robot/constants/RobotConstants.java
@@ -106,7 +106,7 @@ public class RobotConstants {
   }
 
   public static class AutoAlignConfig {
-    public static final double maxDistanceFromTarget = 0.2;
+    public static final double maxDistanceFromTarget = 0.5;
     public static final PathConstraints pathConstraints =
         new PathConstraints(1, 0.75, Math.PI, 4 * Math.PI);
   }

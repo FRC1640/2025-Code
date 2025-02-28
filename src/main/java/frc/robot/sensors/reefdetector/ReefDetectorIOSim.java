1301acdf868d7521825736a410207ca6420efdae
Kgupta011 <gimonika82@gmail.com>
2025-02-27 20:59:38 -0500

    e

--- src/main/java/frc/robot/sensors/reefdetector/ReefDetectorIOSim.java
+++ src/main/java/frc/robot/sensors/reefdetector/ReefDetectorIOSim.java
@@ -1,15 +1,17 @@
 package frc.robot.sensors.reefdetector;
 
-import java.util.function.BooleanSupplier;
 import java.util.function.Supplier;
 
 public class ReefDetectorIOSim implements ReefDetectorIO {
+  private Supplier<Boolean> isReef;
 
+  public ReefDetectorIOSim(Supplier<Boolean> isReef) {
+    this.isReef = isReef;
   }
 
   @Override
   public void updateInputs(ReefDetectorIOInputs inputs) {
     inputs.isConnected = true;
-
+    inputs.isDetecting = isReef.get();
   }
 }

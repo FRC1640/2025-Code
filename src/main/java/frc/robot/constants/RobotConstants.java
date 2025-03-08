ddaedf6c0155893337cd5878bc7d2611e1908eaa
jdugan0 <justin.d.dugan@gmail.com>
2025-03-08 16:59:46 -0500

    Update at 'Sat Mar 08 16:59:46 EST 2025'

--- src/main/java/frc/robot/constants/RobotConstants.java
+++ src/main/java/frc/robot/constants/RobotConstants.java
@@ -290,7 +290,7 @@ public class RobotConstants {
 
   public static class ReefDetectorConstants {
     public static final int channel = new RobotSwitch<Integer>(15).get();
-    public static final double detectionThresh = 560;
+    public static final double detectionThresh = 590;
     public static final int averageLength = 20;
     public static final double averagePercentage = 0.8;
     public static final double waitTimeSeconds = 0.1;

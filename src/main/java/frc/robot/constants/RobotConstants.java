5c99f16399842c7301ff6e41dc2b92dc1e6c22e0
jdugan0 <justin.d.dugan@gmail.com>
2025-03-07 20:52:44 -0500

    Update at 'Fri Mar 07 20:52:44 EST 2025'

--- src/main/java/frc/robot/constants/RobotConstants.java
+++ src/main/java/frc/robot/constants/RobotConstants.java
@@ -206,7 +206,7 @@ public class RobotConstants {
     public static final double gearRatio = 5;
     public static final Limits liftLimits = new Limits(0.0, 0.575);
     public static final double liftMaxSpeed = 2;
-    public static final double liftMaxAccel = 1.75;
+    public static final double liftMaxAccel = 1.5;
     public static final TrapezoidProfile.Constraints constraints =
         new TrapezoidProfile.Constraints(liftMaxSpeed, liftMaxAccel);
     public static final double sprocketRadius = Units.inchesToMeters(1.5 / 2);
@@ -224,8 +224,8 @@ public class RobotConstants {
       RightL2(0.112, 0.298, GantrySetpoint.RIGHT),
       LeftL3(0.285, 0.486, GantrySetpoint.LEFT),
       RightL3(0.285, 0.486, GantrySetpoint.RIGHT),
-      LeftL4(0.566, GantrySetpoint.LEFT),
-      RightL4(0.566, GantrySetpoint.RIGHT),
+      LeftL4(0.567, GantrySetpoint.LEFT),
+      RightL4(0.567, GantrySetpoint.RIGHT),
       Trough(0, GantrySetpoint.RIGHT);
 
       public final double lift;

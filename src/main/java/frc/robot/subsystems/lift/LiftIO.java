127374179ed768807f168a116536b49ad1723e3c
SpeedSlicer <mathewshaun25@gmail.com>
2025-03-16 14:48:58 -0400

    commiting for kibash

--- src/main/java/frc/robot/subsystems/lift/LiftIO.java
+++ src/main/java/frc/robot/subsystems/lift/LiftIO.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems.lift;
 
 import org.littletonrobotics.junction.AutoLog;
 
+
 public interface LiftIO extends AutoCloseable {
   @AutoLog
   public static class LiftIOInputs {
@@ -52,4 +53,6 @@ public interface LiftIO extends AutoCloseable {
   public default void testMethod() {}
 
   public default void setLimitEnabled(boolean enable) {}
+  
+  public default void updateEMA(double data){}
 }

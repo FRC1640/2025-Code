91a1e137de7fccaba5606469c20fcd622479c439
SpeedSlicer <mathewshaun25@gmail.com>
2025-03-26 19:20:05 -0400

    uhhhhhhhhhhhhhhh recursive go boom

--- src/main/java/frc/robot/sensors/reefdetector/ReefDetectorIOTOFSensor.java
+++ src/main/java/frc/robot/sensors/reefdetector/ReefDetectorIOTOFSensor.java
@@ -5,22 +5,24 @@ import frc.robot.constants.RobotConstants.ReefDetectorConstants;
 
 public class ReefDetectorIOTOFSensor implements ReefDetectorIO {
   public Counter sensor;
-
+  
   public ReefDetectorIOTOFSensor() {
     sensor = new Counter(ReefDetectorConstants.sensorTOFChannel);
-    
   }
   /**
-   * Returns between 0 - 100, 0 being not detecting, and the more up, the more right it is detecting
+   * Returns between 0 - 255, 0 being not detecting, and the more up, the more right it is detecting
    * @return
    */
-  public double getResolverValue(){
-    double pulseWidth = getResolverValue();
+  public double getValue(){
+    double pulseWidth = sensor.getPeriod();
     return pulseWidth;  
+  }
+  public boolean isReefDetected(){
+    return getValue() != 255 && getValue() != 0;
   }
   
   public double getLocation(){
-    //TODO 
+    // TODO 
     return 0.0;
   }
   @Override

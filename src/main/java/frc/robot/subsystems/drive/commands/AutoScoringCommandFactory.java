9d904863ec60aba98298b4e2657d99c8fa0cc798
1023941311414 <jake.widmann@outlook.com>
2025-03-25 20:06:55 -0400

    lift preset checking in gantry align

--- src/main/java/frc/robot/subsystems/drive/commands/AutoScoringCommandFactory.java
+++ src/main/java/frc/robot/subsystems/drive/commands/AutoScoringCommandFactory.java
@@ -42,7 +42,9 @@ public class AutoScoringCommandFactory {
 
   public Command gantryAlignCommand(Supplier<CoralPreset> getPreset, Supplier<Pose2d> getPose) {
     return gantryCommandFactory.gantryPIDCommand(
-        () -> gantryCommandFactory.getSetpointOdometry(getPreset, getPose));
+        () ->
+            gantryCommandFactory.getSetpointOdometry(
+                getPreset, getPose, () -> liftSubsystem.isAtPreset(getPreset.get().getLift())));
   }
 
   public Command autoPlace() {

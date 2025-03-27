b2bead5ae840ad89aad9d51ead95de9ec503fffa
zoe99999999 <zoe@ebers.com>
2025-03-26 20:10:28 -0400

    mid is like halfway there

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -924,6 +924,7 @@ public class RobotContainer {
 
     NamedCommands.registerCommand(
         "HoldAlgae", algaeCommandFactory.setMotorVoltages(() -> 0.5, () -> 0.5));
+    NamedCommands.registerCommand("Process", algaeCommandFactory.processCommand());
     NamedCommands.registerCommand("RunBackCoral", coralOuttakeCommandFactory.runBack());
     NamedCommands.registerCommand(
         "WaitForCoral",

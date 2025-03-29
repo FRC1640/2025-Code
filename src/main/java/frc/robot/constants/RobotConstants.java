0cf7c67da62f4aad24628a30877f97641679a765
1023941311414 <jake.widmann@outlook.com>
2025-03-29 16:16:52 -0400

    New length constants for align

--- src/main/java/frc/robot/constants/RobotConstants.java
+++ src/main/java/frc/robot/constants/RobotConstants.java
@@ -29,6 +29,7 @@ public class RobotConstants {
   public class RobotDimensions {
     public static final double robotWidth = 0.92;
     public static final double robotLength = 0.92; // 0.927
+    public static final double robotLengthLocalAlign = 0.79;
     public static final Translation2d robotXY = new Translation2d(robotWidth / 2, robotLength / 2);
   }
 

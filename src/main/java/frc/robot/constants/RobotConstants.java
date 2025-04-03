5a9fecb9fb21d7dcc3f75301139d7387c45227b7
jdugan0 <justin.d.dugan@gmail.com>
2025-04-03 16:37:40 -0400

    Update at 'Thu Apr 03 16:37:40 EDT 2025'

--- src/main/java/frc/robot/constants/RobotConstants.java
+++ src/main/java/frc/robot/constants/RobotConstants.java
@@ -365,7 +365,7 @@ public class RobotConstants {
   public static class GantryConstants {
     public static final int gantrySparkID = new RobotSwitch<Integer>(17).get();
     public static final double gantryGearRatio = 27.4;
-    public static final double pulleyRadius = Units.inchesToMeters(0.5) * 1.13278894472;
+    public static final double pulleyRadius = Units.inchesToMeters(0.5) * 1.13278894472 * 0.60103626943;
     // left -> right limit
     public static final Limits gantryLimits = new Limits(0.01, 0.36);
     public static final double gantryLimitCenter = 0.2;

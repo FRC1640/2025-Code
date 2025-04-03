cb712aeeb939dcaa9f44cc200d8a6265a0846ead
jdugan0 <justin.d.dugan@gmail.com>
2025-04-03 18:12:16 -0400

    Update at 'Thu Apr 03 18:12:16 EDT 2025'

--- src/main/java/frc/robot/constants/RobotPIDConstants.java
+++ src/main/java/frc/robot/constants/RobotPIDConstants.java
@@ -105,9 +105,9 @@ public class RobotPIDConstants {
   public static final PIDConstants gantryPID = new PIDConstants(80, 1, 0);
   public static final PIDConstants gantryProfiledPID =
       new PIDConstants(2, 0, 0); // PLACEHOLDERRRRRRRRR
-  public static final PIDConstants liftPID = new PIDConstants(15, 80, 0);
+  public static final PIDConstants liftPID = new PIDConstants(30, 80, 0);
   public static final PIDConstants liftProfiledPIDConstants =
-      new PIDConstants(45, 0, 0); // new PIDConstants(27.25, 0.010569);
+      new PIDConstants(55, 0, 0); // new PIDConstants(27.25, 0.010569);
   public static final PIDConstants liftVelocityPID = new PIDConstants(0, 0, 0);
   public static final FeedForwardConstants liftFF =
       new FeedForwardConstants(0.50579, 3.3, 0, 0.45651); // kv 5.2952

33442d9bc6e13b118e2bca65edf9a33a618d5e77
jdugan0 <justin.d.dugan@gmail.com>
2025-04-10 20:28:19 -0400

    tuning

--- src/main/java/frc/robot/constants/RobotConstants.java
+++ src/main/java/frc/robot/constants/RobotConstants.java
@@ -288,9 +288,9 @@ public class RobotConstants {
       RightL2(0.115, 0.3, GantrySetpoint.RIGHT),
       LeftL3(0.289, 0.486, GantrySetpoint.LEFT),
       RightL3(0.289, 0.486, GantrySetpoint.RIGHT),
-      LeftL4(0.568, GantrySetpoint.LEFT),
+      LeftL4(0.573, GantrySetpoint.LEFT),
       PreMove(0.1, 0.1, GantrySetpoint.CENTER),
-      RightL4(0.568, GantrySetpoint.RIGHT),
+      RightL4(0.573, GantrySetpoint.RIGHT),
       Trough(0.06, GantrySetpoint.CENTER);
 
       public final double lift;

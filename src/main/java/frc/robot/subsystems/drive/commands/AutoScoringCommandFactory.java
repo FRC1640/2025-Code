78e3a84d6b5aaea030b799ee7f2f35e019f30689
jdugan0 <justin.d.dugan@gmail.com>
2025-04-18 18:59:58 -0400

    Update at 'Fri Apr 18 18:59:58 EDT 2025'

--- src/main/java/frc/robot/subsystems/drive/commands/AutoScoringCommandFactory.java
+++ src/main/java/frc/robot/subsystems/drive/commands/AutoScoringCommandFactory.java
@@ -51,7 +51,7 @@ public class AutoScoringCommandFactory {
                 .outtake()
                 .repeatedly()
                 .until(() -> !coralOuttakeSubsystem.hasCoral()))
-        .andThen(coralOuttakeCommandFactory.outtake().repeatedly().withTimeout(0.5))
+        .andThen(coralOuttakeCommandFactory.outtake().repeatedly().withTimeout(0.3))
         .finallyDo(() -> coralOuttakeCommandFactory.outtaking = false);
   }
 

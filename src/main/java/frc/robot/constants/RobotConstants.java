4d35a9c8e7c342df2574f345693bf819a8a69817
SpeedSlicer <mathewshaun25@gmail.com>
2025-05-10 14:59:47 -0400

    bananas

--- src/main/java/frc/robot/constants/RobotConstants.java
+++ src/main/java/frc/robot/constants/RobotConstants.java
@@ -62,45 +62,63 @@ public class RobotConstants {
   }
 
   public class RobotConfigConstants {
-    public static final RobotType robotType = RobotType.Prime24;
+    public static final RobotType robotType = RobotType.Prime25;
     // subsystems
     public static final boolean gantrySubsystemEnabled =
         new RobotSwitch<Boolean>(true)
-            .addValue(RobotType.Prime24, true)
+            .addValue(RobotType.Prime25, true)
+            .addValue(RobotType.Deux25, true)
+            .addValue(RobotType.Prime24, false)
             .addValue(RobotType.Deux24, false)
             .get();
 
     public static final boolean liftSubsystemEnabled =
         new RobotSwitch<Boolean>(true)
-            .addValue(RobotType.Prime24, true)
+            .addValue(RobotType.Prime25, true)
+            .addValue(RobotType.Deux25, true)
+            .addValue(RobotType.Prime24, false)
             .addValue(RobotType.Deux24, false)
             .get();
     public static final boolean algaeIntakeEnabled =
         new RobotSwitch<Boolean>(true)
+            .addValue(RobotType.Prime25, true)
+            .addValue(RobotType.Deux25, true)
+            .addValue(RobotType.Prime24, false)
             .addValue(RobotType.Deux24, false)
-            .addValue(RobotType.Prime24, true)
             .get();
 
     public static final boolean coralOuttakeSubsystemEnabled =
         new RobotSwitch<Boolean>(true)
-            .addValue(RobotType.Prime24, true)
+            .addValue(RobotType.Prime25, true)
+            .addValue(RobotType.Deux25, true)
+            .addValue(RobotType.Prime24, false)
             .addValue(RobotType.Deux24, false)
             .get();
 
     public static final boolean climberSubsystemEnabled =
         new RobotSwitch<Boolean>(true)
+            .addValue(RobotType.Prime25, true)
+            .addValue(RobotType.Deux25, true)
+            .addValue(RobotType.Prime24, false)
             .addValue(RobotType.Deux24, false)
-            .addValue(RobotType.Prime24, true)
             .get();
     // sensors
     public static final boolean reefDetectorEnabled =
         new RobotSwitch<Boolean>(true)
-            .addValue(RobotType.Prime24, true)
+            .addValue(RobotType.Prime25, true)
+            .addValue(RobotType.Deux25, true)
+            .addValue(RobotType.Prime24, false)
             .addValue(RobotType.Deux24, false)
             .get();
 
     // odometry
-    public static final boolean gyroEnabled = new RobotSwitch<Boolean>(true).get();
+    public static final boolean gyroEnabled =
+        new RobotSwitch<Boolean>(true)
+            .addValue(RobotType.Prime25, true)
+            .addValue(RobotType.Deux25, true)
+            .addValue(RobotType.Prime24, true)
+            .addValue(RobotType.Deux24, true)
+            .get();
   }
 
   public static enum PivotId {
